c0cda8b3165cfcde822465e447ed1c1117a39f73
dnov09 <dngige9@gmail.com>
2020-04-11 11:58:07 -0500

    added happyNumber solution

--- LeetCode/30 Days of Leetcode/singleNumber.py
+++ LeetCode/30 Days of Leetcode/singleNumber.py
@@ -1,3 +1,5 @@
+import timeit
+
 '''
 Given a non-empty array of integers, every element appears twice except for one. 
 Find that single one.
@@ -22,6 +24,14 @@ def singleNumber(nums):
         if v == 1:
             return k
 
+def single_number_xor(nums):
+    result = 0
+    for n in nums:
+        result ^= n
+    return result
+
+
 input = [4,1,2,1,2]
 
-print(singleNumber(input))
+timeit.timeit(singleNumber(input))
+timeit.timeit(single_number_xor(input))
