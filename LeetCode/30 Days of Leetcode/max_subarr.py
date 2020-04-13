c5de6073af8e5d981fb118b186d272ce6c02733a
dnov09 <dngige9@gmail.com>
2020-04-12 22:38:18 -0500

    added problem description

---
+++ LeetCode/30 Days of Leetcode/max_subarr.py
@@ -0,0 +1,30 @@
+'''
+Given an integer array nums, 
+find the contiguous subarray (containing at least one number) 
+which has the largest sum and return its sum.
+
+Example:
+
+Input: [-2,1,-3,4,-1,2,1,-5,4],
+Output: 6
+Explanation: [4,-1,2,1] has the largest sum = 6.
+
+
+Initialize:
+    max_so_far = 0
+    max_ending_here = 0
+
+Loop for each element of the array
+  (a) max_ending_here = max_ending_here + a[i]
+  (b) if(max_ending_here < 0)
+            max_ending_here = 0
+  (c) if(max_so_far < max_ending_here)
+            max_so_far = max_ending_here
+return max_so_far
+
+'''
+
+def maxSubArray(nums):
+    pass
+
+print(maxSubArray([1,2,3]))
