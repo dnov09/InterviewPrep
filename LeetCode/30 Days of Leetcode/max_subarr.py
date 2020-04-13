afd15c1409160187e09cb3a4cd0b6d7863abc22f
dnov09 <dngige9@gmail.com>
2020-04-12 22:56:21 -0500

    deleted some stuff

--- LeetCode/30 Days of Leetcode/max_subarr.py
+++ LeetCode/30 Days of Leetcode/max_subarr.py
@@ -9,19 +9,6 @@ Input: [-2,1,-3,4,-1,2,1,-5,4],
 Output: 6
 Explanation: [4,-1,2,1] has the largest sum = 6.
 
-
-Initialize:
-    max_so_far = 0
-    max_ending_here = 0
-
-Loop for each element of the array
-  (a) max_ending_here = max_ending_here + a[i]
-  (b) if(max_ending_here < 0)
-            max_ending_here = 0
-  (c) if(max_so_far < max_ending_here)
-            max_so_far = max_ending_here
-return max_so_far
-
 '''
 
 def maxSubArray(nums):
